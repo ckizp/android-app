0f2457cdac646e33cc1d211e9facf9e6a8dc9816
Elouan Tailliez <lemailaelou@gmail.com>
2024-03-12 11:21:19 -0400

    signup & login ok

--- app/src/main/java/uqac/dim/eventmatch/LoginActivity.java
+++ app/src/main/java/uqac/dim/eventmatch/LoginActivity.java
@@ -2,13 +2,66 @@ package uqac.dim.eventmatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText email;
+    private EditText password;
+    private Button login;
+    private TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+        login = findViewById(R.id.login_button);
+        signUp = findViewById(R.id.switch_signup);
+
+
+        login.setOnClickListener(v -> {
+            String emailText = email.getText().toString().trim();
+            String passwordText = password.getText().toString().trim();
+
+            if(emailText.isEmpty()) {
+                email.setError("Email is required");
+            }
+            if(passwordText.isEmpty()) {
+                password.setError("Password is required");
+            }
+            else{
+                auth.signInWithEmailAndPassword(emailText, passwordText)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "User logged in", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        signUp.setOnClickListener(v -> {
+            Intent signup = new Intent(LoginActivity.this, SignUpActivity.class);
+            signup.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(signup);
+        });
+
+
     }
 }
--- app/src/main/java/uqac/dim/eventmatch/SignUpActivity.java
+++ app/src/main/java/uqac/dim/eventmatch/SignUpActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +18,8 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText password;
     private Button signUp;
 
+    private TextView login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +30,7 @@ public class SignUpActivity extends AppCompatActivity {
         email = findViewById(R.id.signup_email);
         password = findViewById(R.id.signup_password);
         signUp = findViewById(R.id.signup_button);
+        login = findViewById(R.id.switch_login);
 
         signUp.setOnClickListener(v -> {
             String emailText = email.getText().toString().trim();
@@ -43,7 +47,9 @@ public class SignUpActivity extends AppCompatActivity {
                         .addOnCompleteListener(task -> {
                             if (task.isSuccessful()) {
                                 Toast.makeText(SignUpActivity.this, "User created", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                                Intent startActivity = new Intent(SignUpActivity.this, MainActivity.class);
+                                startActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(startActivity);
                             } else {
                                 Toast.makeText(SignUpActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             }
@@ -51,6 +57,12 @@ public class SignUpActivity extends AppCompatActivity {
             }
 
 
+        });
+
+        login.setOnClickListener(v -> {
+            Intent login = new Intent(SignUpActivity.this, LoginActivity.class);
+            login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(login);
         });
     }
 }
