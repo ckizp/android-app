bfe7e9cf5fd257f4721021bf31866238ff7bad5d
Elouan Tailliez <lemailaelou@gmail.com>
2024-04-24 17:21:06 -0400

    Fav view

---
+++ app/src/main/java/uqac/dim/eventmatch/adapters/FavoritesAdapter.java
@@ -0,0 +1,127 @@
+// MyEventListAdapter.java
+package uqac.dim.eventmatch.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import uqac.dim.eventmatch.R;
+import uqac.dim.eventmatch.models.Event;
+import uqac.dim.eventmatch.ui.fragments.main.EventDetailsFragment;
+
+
+public class FavoritesAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<Event> eventList;
+
+    private FirebaseStorage storage;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private FirebaseFirestore database = FirebaseFirestore.getInstance();
+
+    public FavoritesAdapter(Context context, ArrayList<Event> eventList) {
+        this.context = context;
+        this.eventList = eventList;
+        this.storage = FirebaseStorage.getInstance();
+    }
+
+    @Override
+    public int getCount() {
+        return eventList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return eventList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.event_item, parent, false);
+        }
+
+        Event event = eventList.get(position);
+
+        // Récupération des vues
+        TextView eventNameTextView = convertView.findViewById(R.id.text_event_name);
+        ImageView imageView = convertView.findViewById(R.id.image_view);
+        TextView eventDateTextView = convertView.findViewById(R.id.start_date);
+        TextView participantsTextView = convertView.findViewById(R.id.text_participants_count);
+        Button DetailsButton = convertView.findViewById(R.id.button_details);
+        Button EditButton = convertView.findViewById(R.id.button2);
+        EditButton.setText("Enlever");
+
+
+
+
+
+        event.load_image(imageView,storage);
+        eventNameTextView.setText(event.getName());
+        eventDateTextView.setText(event.getStartDate().toDate().toString());
+        participantsTextView.setText(String.valueOf(event.getParticipants().size()));
+        //TODO : Changer le fond du truc
+
+        DetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new EventDetailsFragment(event);
+                FragmentTransaction transaction = ((FragmentActivity) context).getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_layout, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        EditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DocumentReference userReference = database.collection("users").document(user.getUid());
+                userReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            List<DocumentReference> favorites = (List<DocumentReference>) document.get("favorites");
+                            favorites.remove(position);
+                            eventList.remove(position);
+                            userReference.update("favorites", favorites);
+                        }
+                    }
+                });
+            }
+
+
+
+        });
+
+
+
+        return convertView;
+    }
+}
--- app/src/main/java/uqac/dim/eventmatch/ui/fragments/main/FavoritesFragment.java
+++ app/src/main/java/uqac/dim/eventmatch/ui/fragments/main/FavoritesFragment.java
@@ -1,40 +1,56 @@
 package uqac.dim.eventmatch.ui.fragments.main;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
+import com.google.firebase.firestore.GeoPoint;
 import java.util.ArrayList;
 import java.util.List;
-
 import uqac.dim.eventmatch.R;
-
-
+import uqac.dim.eventmatch.adapters.FavoritesAdapter;
+import uqac.dim.eventmatch.adapters.MyEventListAdapter;
+import uqac.dim.eventmatch.models.Event;
 
 
 public class FavoritesFragment extends Fragment {
     private ArrayList<DocumentReference> favoritesList;
     private FirebaseFirestore database;
     private FirebaseUser user;
+    private ListView eventListView;
+    private ArrayList<Event> eventList;
+    private View rootView;
     public FavoritesFragment() {
         user = FirebaseAuth.getInstance().getCurrentUser();
         database = FirebaseFirestore.getInstance();
 
         favoritesList = new ArrayList<>();
+
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        rootView = inflater.inflate(R.layout.fragment_favorites, container, false);
+
+        eventListView = rootView.findViewById(R.id.my_favorits);
+
+        eventList = new ArrayList<Event>();
 
         database.collection("users").document(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -45,17 +61,40 @@ public class FavoritesFragment extends Fragment {
                         favoritesList.add(ref);
                     }
                 }
+                if(!favoritesList.isEmpty()) {
+                    for(DocumentReference ref : favoritesList){
+                        ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot document = task.getResult();
+                                    String name = document.getString("name");
+                                    Timestamp startDate = document.getTimestamp("startDate");
+                                    Timestamp endDate = document.getTimestamp("endDate");
+                                    int participantsCount = document.getDouble("participantsCount").intValue();
+                                    String tags = document.getString("tags");
+                                    List<DocumentReference> partlist = (List<DocumentReference>) document.get("participants");
+                                    String imageUrl = document.getString("imageDataUrl");
+                                    GeoPoint location = new GeoPoint(document.getGeoPoint("location").getLatitude(), document.getGeoPoint("location").getLongitude());
+                                    String description = document.getString("description");
+                                    DocumentReference owner = document.getDocumentReference("owner");
+                                    Event event = new Event(name, endDate, startDate, participantsCount, tags, partlist, imageUrl,owner, location, description);
+
+                                    eventList.add(event);
+                                }
+                                FavoritesAdapter customBaseAdapter = new FavoritesAdapter(rootView.getContext(), eventList);
+                                Log.i("FAVORIS", "eventList size : " + eventList.size());
+                                eventListView.setAdapter(customBaseAdapter);
+                                Log.i("FAVORIS", "eventListView size : " + eventListView.getCount());
+                            }
+                        });
+                    }
+
+                }
+
             }
         });
 
-
-
-
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        return rootView;
     }
 }
