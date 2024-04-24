8abcd4dfd2753a8158490cb5c79110330f226017
JoshuaMrtl <joshua.martinoli@etu.univ-cotedazur.fr>
2024-04-24 13:38:01 -0400

    suppression bouton jour/nuit

--- app/src/main/java/uqac/dim/eventmatch/ui/fragments/main/ProfileFragment.java
+++ app/src/main/java/uqac/dim/eventmatch/ui/fragments/main/ProfileFragment.java
@@ -3,19 +3,15 @@ package uqac.dim.eventmatch.ui.fragments.main;
 import static android.content.Context.MODE_PRIVATE;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.navigation.NavigationView;
@@ -25,7 +21,6 @@ import java.util.Objects;
 
 import uqac.dim.eventmatch.R;
 import uqac.dim.eventmatch.ui.activities.LoginActivity;
-import uqac.dim.eventmatch.ui.activities.SignUpActivity;
 import uqac.dim.eventmatch.ui.fragments.profile.AccountFragment;
 import uqac.dim.eventmatch.ui.fragments.profile.FeedbackFragment;
 import uqac.dim.eventmatch.ui.fragments.profile.MyEventsFragment;
@@ -56,16 +51,6 @@ public class ProfileFragment extends Fragment
         TextView TxtProfileName = view.findViewById(R.id.nom_profile);
         //Display du nom de l'utilisateur
         TxtProfileName.setText(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getDisplayName());
-
-        ToggleButton themeToggle = view.findViewById(R.id.theme_toggle);
-
-        themeToggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-            }
-        });
 
         return view;
     }
